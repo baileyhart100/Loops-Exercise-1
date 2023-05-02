d86c46d3dbda82acbc62ed13f2f558ebd9a3291f
Bailey Hart <baileyhart100@gmail.com>
2023-05-02 11:00:13 -0400

    Add project files.

---
+++ Loops Exercise 1/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i <5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i <5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i <5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//    {
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
